376561ae9b3ffe44bf870ad0d2200c3c209608f2
Sean L <slawrence@dvrpc.org>
2021-06-30 16:18:50 -0400

    remove discord webhook for security purposes

--- shapefile_count.py
+++ shapefile_count.py
@@ -6,7 +6,7 @@ for root, dirs, files in os.walk('u:'):
         if file.endswith('.shp'):
             count += 1
 data = {"embeds": [{"image": {"url": "http://apimeme.com/meme?meme=But-Thats-None-Of-My-Business&top="+str(count)+"+shapefiles+on+the+U+drive&bottom=but+that+none+of+my+business"}}]}
-result = requests.post(url='https://discord.com/api/webhooks/842202207146344490/ucowiXCNb7-r_Mra0X0NtG_LaGO70xhhEWu4C-jx3tA2oXGn6DHoZM8Sl5pAcTqElzXE', json=data)
+result = requests.post(url='discord webhook', json=data)
 try:
     result.raise_for_status()
 except requests.exceptions.HTTPError as err:
